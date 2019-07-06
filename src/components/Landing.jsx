a7393ee7ffca12f205162141d6f8fa6d811775ed
Cristian villanueva <cristianvillanueva@Cristians-MacBook-Pro-2.local>
2019-07-05 20:31:00 -0500

    landing section basics complete

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,12 +3,31 @@ import "./Landing-style.css";
 
 const Landing = () => {
   return (
-    <div className="landing-editorTheme">
-      <p>1 2 3 4 5 6 7 8 9 10 11</p>
-      <h1> &#123; CRISTIAN VILLANUEVA &#125; </h1>
-      <a href="/">Software </a>
-      <a href="/">Videography </a>
-      <a href="/">Design </a>
+    <div className="landing-editorTheme flex-container">
+      <div className="flex-container linecounter">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
+        <p>6</p>
+        <p>7</p>
+        <p>8</p>
+        <p>9</p>
+        <p>10</p>
+        <p>11</p>
+        <p>12</p>
+        <p>13</p>
+        <p>14</p>
+        <p>15</p>
+      </div>
+
+      <div className="landing-flex">
+        <h1 className="landing-text"> &#123; CRISTIAN VILLANUEVA &#125; </h1>
+        <a href="/">Software </a>
+        <a href="/">Videography </a>
+        <a href="/">Design </a>
+      </div>
     </div>
   );
 };
